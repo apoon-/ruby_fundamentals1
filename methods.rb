b556acb841eaa2f41fb8da243bf9a4f0a96e7224
apoon- <anson.os.poon@gmail.com>
2013-10-21 22:15:21 -0400

    fix

--- methods.rb
+++
@@ -1,7 +0,0 @@
-#  Exercise: Method Acting
-
-# def cat
-#   __callee__
-# end
-
-# p cat
--- operators.rb
+++
@@ -1,9 +0,0 @@
-# Exercise: && Versus And
-
-# p "blue"
-
-# Exercise: Hiring Programmers - Boolean Expressions in Ruby
-
-# is_an_experienced_ruby_programmer = (candidate.languages_worked_with.include? 'Ruby') && (candidate.years_of_experience >= 2 || candidate.github_points >= 500) && !(candidate.age < 15 || candidate.applied_recently?)
-
-
